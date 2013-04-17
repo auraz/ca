9fcf2c0e4c9293b83f0018b0441a2bd0ddbea171
Alexey Machulenko <machulen@gmail.com>
2013-04-17 08:04:21 +0300

    The simple implementation of the cellular automaton is now ready\!

--- test_init.py
+++ test_init.py
@@ -7,12 +7,25 @@ import unittest
 class TestInit(unittest.TestCase):
     
     def test1(self):
-        # Проверить, что функция перехода возвращает правильное новое значение клетки по заданным соседним.
-        # Пусть это будет среднее арифметическое.
-        neighbors = [[1, 2, 3],
+    	"""testing average(neighbors)"""
+        neighbors = [
+        			 [1, 2, 3],
                      [4, 5, 6],
-                     [3, 2, 1]]
-        self.assertEqual(init.transition(neighbors), 3.0)
+                     [3, 2, 1],
+                     		   ]
+        neighbors_border_case = [
+			        			 [1, 2, None],
+			                     [3, 4, None],
+			                     [5, 6, None],
+			                     		      ]
+        neighbors_corner_case = [
+			        			 [None,  1  ,  2  ],
+			                     [None,  3  ,  4  ],
+			                     [None, None, None],
+			                     		            ]
+        self.assertEqual(init.average(neighbors), 3.0)
+        self.assertEqual(init.average(neighbors_border_case), 3.5)
+        self.assertEqual(init.average(neighbors_corner_case), 2.5)
 
 if __name__ == "__main__":
 	suite = unittest.TestLoader().loadTestsFromTestCase(TestInit)
