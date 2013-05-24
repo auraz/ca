2a181ada82602b30e82ba8935e825d2bcfff6b63
Alexey Machulenko <machulen@gmail.com>
2013-05-24 17:22:46 +0300

    Start refactoring

--- ca.py
+++ ca.py
@@ -6,6 +6,7 @@ import init
 import field
 import copy
 
+
 class CellularAutomaton():
     """Простая реализация клеточного автомата.
 
@@ -48,54 +49,15 @@ class CellularAutomaton():
         self.height = field.height()
         # Сделаем копию, иначе переданное извне поле будет меняться
         self.field = copy.deepcopy(field)
-        self.__surround(self.field)
+        self.field.surround()
 
-    def __surround(self, grid):
-        """Окружает поле слоем из None со всех сторон.
-
-            grid - поле.
-
-            Меняет само поле. Возвращает None.
-            """
-
-        # Получаем ширину и высоту списка:
-        w = grid.width()
-        h = grid.height()
-
-        for row in grid:
-            row[w:w] = [None]   # справа
-            row[0:0] = [None]   # слева
-
-        grid[h:h] = [[None for i in range(w + 2)]]   # снизу
-        grid[0:0] = [grid[h]]                        # сверху
-    
-    def neighbors(self, x, y):
-        """Возвращает всех соседей заданной клетки.
-
-            x и y - координаты клетки.
-
-            Возвращает двухмерный список 3 х 3, содержащий значения
-            заданной клетки и 8 её соседей. При этом сама заданная
-            клетка всегда будет по центру.
-            Если клетка находится у границы клеточного автомата,
-            часть соседей будет иметь значение на None.
-            Например, если метод вернул
-
-            None None None
-            None   1    2
-            None   4    5
-
-            , то это значит, что клетка находится в левом верхнем углу.
-            """
-        return self.field.get_subfield(x, y, 3, 3)
-
-    def get_field(self):
-        """Возвращает все клетки клеточного автомата"""
-        return self.field.get_subfield(1, 1, self.width, self.height)
+    # def get_field(self):
+    #     """Возвращает все клетки клеточного автомата"""
+    #     return self.field.get_subfield(1, 1, self.width, self.height)
 
     def next_cell_value(self, x, y):
         """Возвращает следующее значение клетки с координатами x и y."""
-        return self.next_value(self.neighbors(x, y))
+        return self.next_value(self.field.neighbors(x, y))
     
     def next(self):
         """Выполняет одну итерацию клеточного автомата."""
@@ -103,23 +65,19 @@ class CellularAutomaton():
                                 for i in range(self.width)] 
                                     for j in range(self.height)])
         self.field = new_field
-        self.__surround(self.field)
+        self.field.surround()
     
 
-
-def print_2d(seq):
-    """Выводит двухмерную последовательность построчно."""
-    for row in seq:
-        print row
 
 
 if __name__ == '__main__':
     ca = CellularAutomaton()
     print
     print "Initial state of the cellular automaton:"
-    print_2d(ca.get_field())
+    print ca.field
     print
     print "After one iteration:"
     ca.next()
-    print_2d(ca.get_field())
+    print ca.field
+    # print_2d(ca.get_field())
     print
