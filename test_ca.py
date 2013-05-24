6333d4cddccb51c89d59492b805492913631467e
Alexey Machulenko <machulen@gmail.com>
2013-05-24 19:55:21 +0300

    Fix tests

--- test_ca.py
+++ test_ca.py
@@ -13,72 +13,6 @@ class TestCellularAutomaton(unittest.TestCase):
         self.ca = CellularAutomaton(field, init.average)
     
     def test1(self):
-        """testing the get_field() method ..."""
-        self.assertEqual(self.ca.get_field(), field)
-    
-    # def test2(self):
-    #     """testing the get_subfield() method."""
-    #     self.assertEqual(self.ca.get_field().get_subfield(1, 2), [[9]])
-    #     self.assertEqual(self.ca.get_field().get_subfield(1, 0, 4), [[0, 0, 0, 9]])
-    #     self.assertEqual(self.ca.get_field().get_subfield(1, 1, 1, 4), [
-    #                                                         [0],
-    #                                                         [9],
-    #                                                         [0],
-    #                                                         [0],
-    #                                                             ])
-    #     self.assertEqual(self.ca.get_field().get_subfield(0, 2, 3, 4), [
-    #                                                         [0, 9, 0],
-    #                                                         [0, 0, 0],
-    #                                                         [9, 0, 0],
-    #                                                         [0, 0, 9],
-    #                                                                   ])
-    #     # Border cases:
-    #     self.assertEqual(self.ca.get_field().get_subfield(-1, 2, 7), 
-    #                                       [[None, 0, 9, 0, 0, 0, None]])
-
-    #     self.assertEqual(self.ca.get_field().get_subfield(5, -1, 1, 4), [
-    #                                                          [None],
-    #                                                          [None],
-    #                                                          [None],
-    #                                                          [None],
-    #                                                                 ])
-    #     self.assertEqual(self.ca.get_field().get_subfield(-1, 4, 4, 3),
-    #                                         [
-    #                                          [None,  9  ,  0  ,  0  ],
-    #                                          [None,  0  ,  0  ,  9  ],
-    #                                          [None, None, None, None],
-    #                                                                   ])
-    
-    # def test3(self):
-    #     """testing the get_cell() method  ..."""
-    #     self.assertEqual(self.ca.get_cell(0, 0), 0)
-    #     self.assertEqual(self.ca.get_cell(1, 2), 9)
-    #     self.assertEqual(self.ca.get_cell(2, 5), 9)
-    #     self.assertEqual(self.ca.get_cell(-1, -1), None)
-    #     self.assertEqual(self.ca.get_cell(5, 3), None)
-    
-    def test4(self):
-        """testing the neighbors(x, y) method"""
-        self.assertEqual(self.ca.neighbors(1, 3), [[0, 9, 0],
-                                                   [0, 0, 0],
-                                                   [9, 0, 0]])
-        
-        self.assertEqual(self.ca.neighbors(1, 5), [[9,    0,    0],
-                                                   [0,    0,    9],
-                                                   [None, None, None]])
-        
-        self.assertEqual(self.ca.neighbors(4, 0), [[None, None, None],
-                                                   [0,    9,    None],
-                                                   [0,    0,    None]])
-
-    # def test3(self):
-    #     """testing the are_neighbors(x1, y1, x2, y2) method"""
-    #     self.assertTrue(self.ca.are_neighbors(5, 7, 5, 7,))
-    #     self.assertTrue(self.ca.are_neighbors(4, 6, 4, 5,))
-    #     self.assertTrue(self.ca.are_neighbors(2, 8, 1, 9,))
-    #     self.assertFalse(self.ca.are_neighbors(4, 1, 7, 6,))
-    
-    def test5(self):
         """testing the next() method  ... ..."""
         self.ca.next()
         expected = [
@@ -89,7 +23,7 @@ class TestCellularAutomaton(unittest.TestCase):
                     [1.5, 2.0, 1.0, 1.0, 0.0],
                     [2.25, 3.0, 1.5, 1.5, 0.0],
                                                ]
-        self.assertEqual(self.ca.get_field(), expected)
+        self.assertEqual(self.ca.field.get_inner(), expected)
 
 
 if __name__ == '__main__':
--- test_field.py
+++ test_field.py
@@ -7,75 +7,50 @@ class TestField(unittest.TestCase):
 
     def setUp(self):
         self.field = Field([
-                            [0   , 1   , 2   , 3   , 4   ],
-                            [0.5 , 1.4 , 2.3 , 3.2 , 4.1 ],
-                            ['a' , 'b' , 'c' , 'd' , 'e' ],
-                            [None, []  , ()  , {}  , ''  ],
-                            [None, [1] , '23', 4   , 5   ],
-                            [5   , 6   , 7   , 8   , 9   ],
-                                                           ])
+                            [1,  2,  3 ],
+                            [4,  5,  6 ],
+                            [7,  8,  9 ],
+                            [10, 11, 12],
+                                         ])
     
-    def test1_1(self):
-        """testing the  width() method"""
-        self.assertEqual(self.field.width(), 5)
+    def test1(self):
+        """testing the surround() method"""
+        self.field.surround()
+        self.assertEqual(self.field, [
+                                      [None, None, None, None, None],
+                                      [None,  1  ,  2  ,  3  , None],
+                                      [None,  4  ,  5  ,  6  , None],
+                                      [None,  7  ,  8  ,  9  , None],
+                                      [None,  10 ,  11 ,  12 , None],
+                                      [None, None, None, None, None],
+                                                                     ])
 
-    def test1_2(self):
-        """testing the height() method"""
-        self.assertEqual(self.field.height(), 6)
+    def test2(self):
+        """testing the get_inner() method"""
+        self.field.surround()
+        self.assertEqual(self.field.get_inner(), [
+                                                  [1,  2,  3 ],
+                                                  [4,  5,  6 ],
+                                                  [7,  8,  9 ],
+                                                  [10, 11, 12],
+                                                               ])
     
-    def test2_1(self):
-        """testing list features: item access"""
-        self.assertEqual(self.field[4][2], '23')
-        self.assertEqual(self.field[3][0], None)
-        self.assertEqual(self.field[0][4],  4  )
-        self.assertEqual(self.field[5]   , [5, 6, 7, 8, 9])
+    def test3(self):
+        """testing the neighbors(x, y) method"""
+        self.field.surround()
+        self.assertEqual(self.field.neighbors(1, 2), [[4,  5,  6 ],
+                                                   [7,  8,  9 ],
+                                                   [10, 11, 12]])
+        
+        self.assertEqual(self.field.neighbors(0, 1), [[None, 1, 2],
+                                                   [None, 4, 5],
+                                                   [None, 7, 8]])
+        
+        self.assertEqual(self.field.neighbors(2, 0), [[None, None, None],
+                                                   [2,    3,    None],
+                                                   [5,    6,    None]])
 
-    def test2_2(self):
-        """testing list features: slicing ..."""
-        self.assertEqual(self.field[3:5],
-            [
-             [None, []  , ()  , {}  , ''  ],
-             [None, [1] , '23', 4   , 5   ],
-                                            ])
-        self.assertEqual(self.field[2::2],
-            [
-             ['a' , 'b' , 'c' , 'd' , 'e' ],
-             [None, [1] , '23', 4   , 5   ],
-                                            ])
-        self.assertEqual(self.field[0][::2], [0, 2, 4])
-        self.assertEqual(self.field[:],   self.field  )
 
-    def test3_1(self):
-        """testing the get_subfield() method: single cell  ..."""
-        self.assertEqual(self.field.get_subfield(1, 2), [['b']])
-        self.assertEqual(self.field.get_subfield(4, 1), [[4.1]])
-        self.assertEqual(self.field.get_subfield(0, 5), [[ 5 ]])
-    
-    def test3_2(self):
-        """testing the get_subfield() method: single row.. ..."""
-        self.assertEqual(self.field.get_subfield(1, 4, 4),
-                                       [[[1], '23', 4, 5]])
-    
-    def test3_3(self):
-        """testing the get_subfield() method: single column..."""
-        self.assertEqual(self.field.get_subfield(1, 1, 1, 5),
-            [
-             [1.4],
-             ['b'],
-             [[ ]],
-             [[1]],
-             [ 6 ],
-                   ])
-    
-    def test3_4(self):
-        """testing the get_subfield() method: rectangular area"""
-        self.assertEqual(self.field.get_subfield(1, 2, 3, 4), 
-            [
-             ['b' , 'c' , 'd' ],
-             [[]  , ()  , {}  ],
-             [[1] , '23', 4   ],
-             [6   , 7   , 8   ],
-                                ])
 
 
 if __name__ == '__main__':
