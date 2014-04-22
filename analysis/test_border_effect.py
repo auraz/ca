04e06c5c5c83ad2be29421467220f480e301ab19
machulen <machulen@gmail.com>
2014-04-22 17:16:54 +0300

    Add a test for the function test_non_blank_concentration(g, d)

--- analysis/test_border_effect.py
+++ analysis/test_border_effect.py
@@ -5,25 +5,32 @@ from border_effect import *
 
 class TestBorderEffect(unittest.TestCase):
 
+    def setUp(self):
+        self.grid = [
+                     [0, 0, 1, 1, 1, 0, 0, 0, 0, 0],    # ..ooo.....
+                     [0, 0, 1, 2, 1, 0, 0, 0, 0, 0],    # ..o#o.....
+                     [0, 0, 1, 1, 1, 0, 0, 1, 1, 1],    # ..ooo..ooo
+                     [0, 0, 0, 0, 0, 0, 0, 1, 2, 1],    # .......o#o
+                     [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],    # .......ooo
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    # ..........
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    # ..........
+                     [1, 1, 1, 0, 0, 1, 1, 1, 0, 0],    # ooo..ooo..
+                     [1, 1, 1, 0, 0, 1, 2, 1, 0, 0],    # ooo..o#o..
+                     [1, 2, 1, 0, 0, 1, 1, 1, 0, 0],    # o#o..ooo..
+                                                    ]
     def test_non_blank_cells(self):
-        g = [
-             [0, 0, 1, 1, 1, 0, 0, 0, 0, 0],    # ..ooo.....
-             [0, 0, 1, 2, 1, 0, 0, 0, 0, 0],    # ..o#o.....
-             [0, 0, 1, 1, 1, 0, 0, 1, 1, 1],    # ..ooo..ooo
-             [0, 0, 0, 0, 0, 0, 0, 1, 2, 1],    # .......o#o
-             [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],    # .......ooo
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    # ..........
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    # ..........
-             [1, 1, 1, 0, 0, 1, 1, 1, 0, 0],    # ooo..ooo..
-             [1, 1, 1, 0, 0, 1, 2, 1, 0, 0],    # ooo..o#o..
-             [1, 2, 1, 0, 0, 1, 1, 1, 0, 0],    # o#o..ooo..
-                                            ]
-        self.assertEqual(non_blank_cells(g, 0), 14)
-        self.assertEqual(non_blank_cells(g, 1), 12)
-        self.assertEqual(non_blank_cells(g, 2), 10)
-        self.assertEqual(non_blank_cells(g, 3),  0)
-        self.assertEqual(non_blank_cells(g, 4),  0)
+        self.assertEqual(non_blank_cells(self.grid, 0), 14)
+        self.assertEqual(non_blank_cells(self.grid, 1), 12)
+        self.assertEqual(non_blank_cells(self.grid, 2), 10)
+        self.assertEqual(non_blank_cells(self.grid, 3),  0)
+        self.assertEqual(non_blank_cells(self.grid, 4),  0)
 
+    def test_non_blank_concentration(self):
+        self.assertAlmostEqual(non_blank_concentration(self.grid, 0), 14/36.0)
+        self.assertAlmostEqual(non_blank_concentration(self.grid, 1), 12/28.0)
+        self.assertAlmostEqual(non_blank_concentration(self.grid, 2), 0.5)
+        self.assertAlmostEqual(non_blank_concentration(self.grid, 3), 0)
+        self.assertAlmostEqual(non_blank_concentration(self.grid, 4), 0)
 
 
 if __name__ == '__main__':
