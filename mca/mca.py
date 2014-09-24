e180c5dbea8f90f3b9206ebad83105f6e04dadc5
machulen <machulen@users.noreply.github.com>
2014-09-24 23:43:35 +0300

    Rename MCA to Base_MCA, and Model6 to MCA.

--- mca/mca.py
+++ mca/mca.py
@@ -157,6 +157,7 @@ class Nucleus:
         self.right = right
         self.up = up
         self.down = down
+        self.mca.nucleus_cell_value = 7
 
         # Прописываем зародыш в списке зародышей.
         mca.nuclei.append(self)
@@ -418,7 +419,13 @@ class Nucleus:
 
 
 
-class MCA:
+
+
+
+
+
+
+class Base_MCA:
     """Базовый класс для МКА.
 
         МКА содержит:
@@ -447,9 +454,9 @@ class MCA:
         self.gap = gap
 
         # Create a field, initialize it with zeros
-        # Create an empty list of nuclei and fibers
         self.field = numpy.zeros((field_size, field_size), dtype = numpy.int64)
-        # Field([[0 for i in range(field_size)] for i in range(field_size)])
+
+        # Create an empty list of nuclei and fibers
         self.nuclei = []
         self.fibers = []
         self.condemned = []
@@ -468,5 +475,111 @@ class MCA:
         plt.show()
 
 
-        
 
+
+
+class MCA(Base_MCA):
+    """Модель 6.
+        """
+
+    def __init__(self, n, a, f, g1, g2):
+        Base_MCA.__init__(self, n, f, g1)
+        self.g2 = g2
+
+        if isinstance(a, int):
+            self.attempts = a
+        elif isinstance(a, float):
+            self.attempts = int(a * n**2)
+        else:
+            raise TypeError("'a' must be an int or a float.")
+
+
+
+    def run(self, step, plot = False):
+        """Запуск модели."""
+
+        self.spawn_nuclei(step)
+        self.grow_fibers(plot)
+
+
+
+    def spawn_nuclei(self, step = 0.1, plot = False, nucleus_cell_value = 7):
+
+        if isinstance(step, int):
+            pass
+        elif isinstance(step, float):
+            step = int(step * self.attempts)
+        else:
+            raise TypeError("'step' must be an int or a float.")
+
+        self.nucleus_cell_value = nucleus_cell_value
+
+        # Make a list of random numbers without repetitions:
+        random_list = range(self.field_size**2)
+        random.shuffle(random_list)
+
+        for i in range(self.attempts / step):
+            for r in random_list[i * step : (i + 1) * step]:
+                x = r % self.field_size
+                y = r / self.field_size
+                if self.field[y, x] == 0:
+                    nuc = Nucleus(x, y, self)
+            print (i + 1) * step, len(self.nuclei)
+        
+        if plot:
+            self.plot()
+
+
+    
+    def grow_fibers(self, plot = False):
+
+        self.gap = self.g2
+
+        self.field *= 0
+
+        for nuc in self.nuclei:
+            self.field[nuc.y, nuc.x] = 1
+
+        for i in range((self.fiber_size - 1) * 2):
+
+            if plot == "each step":
+                self.plot()
+
+            # growing:
+            for nuc in self.nuclei:
+                nuc.look_around()
+                nuc.try_to_grow(die_safely = True)
+            # killing nuclei safely:
+            for nuc in self.condemned:
+                nuc.die()
+            self.condemned = []
+
+            # reporting:
+            print i + 1, len(self.nuclei)
+
+        concentration = 100.0 * len(self.nuclei) * self.fiber_size**2 / self.field_size**2
+        print "{} * {} / {} * 100% = {}%".format(
+            len(self.nuclei), self.fiber_size**2, self.field_size**2, concentration)
+        print "Fibers take {}%.".format(concentration)
+        self.concentration = concentration
+
+        if plot or (plot == "each step"):
+            self.plot()
+
+
+
+Model6 = MCA     # for backward compatibility
+
+
+
+
+if __name__ == '__main__':
+    m = MCA(
+        n  = 300,
+        a  = 1.0,
+        f  = 8,
+        g1 = 14,
+        g2 = 7
+    )
+    m.spawn_nuclei(plot = True)
+    m.grow_fibers(plot = True)
