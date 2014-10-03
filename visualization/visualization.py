2a944a3a7bcbb6cec5676fa2eb044a9092f8a163
machulen <machulen@users.noreply.github.com>
2014-10-03 18:59:30 +0300

    Remake 2D visualization logic.

--- visualization/visualization.py
+++ visualization/visualization.py
@@ -1,106 +1,65 @@
-# -*- coding: UTF-8 -*-
-
 import sys
 sys.path.append("..")
-from math import *
 from mca.mca import *
+# from visualization.circles import *
 
-def draw_circles(m, zoom = 5):
+from math import pi
+import pickle
 
-    n = m.field_size
-    canvas = numpy.zeros((n * zoom, ) * 2, dtype = numpy.int64)
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
 
-    fibers_centens = []
-    for nuc in m.nuclei:
-        xc = nuc.x - nuc.left * 0.5 + nuc.right * 0.5
-        yc = nuc.y - nuc.up   * 0.5 + nuc.down  * 0.5
-        new_xc = zoom * (xc + 0.5) - 0.5
-        new_yc = zoom * (yc + 0.5) - 0.5
-        fibers_centens.append((new_xc, new_yc))
 
-    r = m.fiber_size / pi**0.5 * zoom
 
-    ci_dots = 0
-    for center in fibers_centens:
-        xc, yc = center
-        left_side   = int(max( ceil(xc - r), 0           ))
-        right_side  = int(min(floor(xc + r), n * zoom - 1))
-        top_side    = int(max( ceil(yc - r), 0           ))
-        bottom_side = int(min(floor(yc + r), n * zoom - 1))
 
-        for y in range(top_side, bottom_side + 1):
-            for x in range(left_side, right_side + 1):
-                if (x - xc)**2 + (y - yc)**2 <= r**2:
-                    canvas[y, x] = 1
-                    ci_dots += 1
+def fibers_centers(mca):
+    centers = []
+    for nuc in mca.nuclei:
+        # explain why you add 0.5!
+        xc = 0.5 + nuc.x - nuc.left * 0.5 + nuc.right * 0.5
+        yc = 0.5 + nuc.y - nuc.up   * 0.5 + nuc.down  * 0.5
+        print xc, yc
+        centers.append((xc, yc))
+    print
+    return centers
 
-    sq_dots = m.fiber_size**2 * len(m.nuclei) * zoom**2
-    print "Circles take", ci_dots * 100. / n**2 / zoom**2, "%."
-    print "Squares:", sq_dots, "dots."
-    print "Circles:", ci_dots, "dots."
-    print "Difference:", 100. * (ci_dots - sq_dots) / sq_dots, "%."
 
-    im2 = plt.imshow(canvas, cmap=cm.gray, interpolation='nearest')
+def shift_centers(centers, n, r):
+    # n = mca.field_size
+    new_centers = []
+    for xc, yc in centers:
+        xc = xc if xc - r > 0 else r
+        xc = xc if xc + r < n else n - r
+        yc = yc if yc - r > 0 else r
+        yc = yc if yc + r < n else n - r
+        print xc, yc
+        new_centers.append((xc, yc))
+    return new_centers
+
+
+def draw_circles(centers, n, r):
+    fig, ax = plt.subplots()
+    ax.set_xlim(0, n)
+    ax.set_ylim(n, 0)
+    ax.set_aspect('equal')
+    for c in centers:
+        circle = mpl.patches.Circle(c, r)
+        ax.add_patch(circle)
+    return fig
+
+
+if __name__ == '__main__':
+    f = open("mca.pickle", 'rb')
+    mca = pickle.load(f)
+    f.close()
+
+    plt.imshow(mca.field, cmap=cm.gray, interpolation='nearest')
+
+    n = mca.field_size
+    r = mca.fiber_size / pi**0.5
+    centers = fibers_centers(mca)
+    shifted_centers = shift_centers(centers, n, r)
+    draw_circles(centers, n, r)
+    draw_circles(shifted_centers, n, r)
     plt.show()
-
-
-
-def shift_and_draw_circles(m, zoom = 5):
-
-    n = m.field_size
-    canvas = numpy.zeros((n * zoom, ) * 2, dtype = numpy.int64)
-    r = m.fiber_size / pi**0.5
-
-    fibers_centens = []
-    for nuc in m.nuclei:
-        xc = nuc.x - nuc.left * 0.5 + nuc.right * 0.5
-        yc = nuc.y - nuc.up   * 0.5 + nuc.down  * 0.5
-
-        xc = xc if xc - r > 0.5     else r-0.5
-        xc = xc if xc + r < n - 0.5 else n - 0.5 - r
-        yc = yc if yc - r > 0.5     else r-0.5
-        yc = yc if yc + r < n - 0.5 else n - 0.5 - r
-        # if xc + r > n:
-        #     print "xc + r > n! xc =", xc, ", r =", r, ", n =", n, ". New value of xc =", n - r
-        #     xc = n - r
-
-        xc = zoom * (xc + 0.5) - 0.5
-        yc = zoom * (yc + 0.5) - 0.5
-        fibers_centens.append((xc, yc))
-
-    r *= zoom
-    
-    ci_dots = 0
-    for center in fibers_centens:
-        xc, yc = center
-        left_side   = int(floor(xc - r))
-        right_side  = int(ceil(xc + r))
-        top_side    = int(floor(yc - r))
-        bottom_side = int(ceil(yc + r))
-        # if xc > 470:
-        #     print xc, yc, left_side, right_side, top_side, bottom_side, r
-        #     for y in range(top_side, bottom_side + 1):
-        #         for x in range(left_side, right_side + 1):
-        #             if (x - xc)**2 + (y - yc)**2 <= r**2:
-        #                 canvas[y, x] = 1
-        #                 ci_dots += 1
-        #                 print '*',
-        #             else:
-        #                 print ' ',
-        #         print
-        # else:
-        for y in range(top_side, bottom_side + 1):
-            for x in range(left_side, right_side + 1):
-                if (x - xc)**2 + (y - yc)**2 <= r**2:
-                    canvas[y, x] = 1
-                    ci_dots += 1
-
-    sq_dots = m.fiber_size**2 * len(m.nuclei) * zoom**2
-    print "Circles take", ci_dots * 100. / n**2 / zoom**2, "%."
-    print "Squares:", sq_dots, "dots."
-    print "Circles:", ci_dots, "dots."
-    print "Difference:", 100. * (ci_dots - sq_dots) / sq_dots, "%."
-
-    im2 = plt.imshow(canvas, cmap=cm.gray, interpolation='nearest')
-    plt.show()
-
