02e00480998dd8d37ab918cbbc19f1afb2c392e6
machulen <machulen@users.noreply.github.com>
2014-10-06 17:43:18 +0300

    Add figure saving and 3D visualization.

--- visualization/visualization.py
+++ visualization/visualization.py
@@ -13,6 +13,12 @@ import numpy as np
 mpl.rcParams['figure.figsize' ] = 6, 6    # figure size in inches
 mpl.rcParams[ 'axes.facecolor'] = 'black'
 mpl.rcParams['patch.facecolor'] = 'white'
+mpl.rcParams['savefig.facecolor'] = 'lightgray'
+mpl.rcParams['image.cmap'] = 'gray'
+mpl.rcParams['image.interpolation'] = 'nearest'
+
+
+
 
 def fibers_centers(mca):
     centers = []
@@ -50,17 +56,62 @@ def draw_circles(centers, n, r):
     return fig
 
 
+def draw_cyllindres(centers, n, r, h, **kwargs):
+    # h = 2000
+    t = 90
+    # dphi = 2*pi / t
+    phi = numpy.linspace(0, 2*pi, t + 1)
+    # phi = np.linspace(0, 2*pi)
+
+    x, y, z = [], [], []
+    for xc, yc in centers:
+        x.append(np.array((xc + np.zeros(t + 1),
+                           xc + r * np.cos(phi),
+                           xc + r * np.cos(phi),
+                           xc + np.zeros(t + 1))))
+
+        y.append(np.array((yc + np.zeros(t + 1),
+                           yc + r * np.sin(phi),
+                           yc + r * np.sin(phi),
+                           yc + np.zeros(t + 1))))
+
+        z.append(np.array((np.zeros(t + 1),
+                           np.zeros(t + 1),
+                           np.ones(t + 1) * h,
+                           np.ones(t + 1) * h)))
+
+
+    x = np.vstack(x)
+    y = np.vstack(y)
+    z = np.vstack(z)
+
+
+    mlab.mesh(x, y, z, **kwargs)
+
+
+
 if __name__ == '__main__':
     f = open("mca.pickle", 'rb')
     mca = pickle.load(f)
     f.close()
 
-    plt.imshow(mca.field, cmap=cm.gray, interpolation='nearest')
+    # plt.imshow(mca.field, cmap=cm.gray, interpolation='nearest')
+    plt.imshow(mca.field)
+    plt.savefig('figure1.png')
 
     n = mca.field_size
     r = mca.fiber_size / pi**0.5
+
     centers = fibers_centers(mca)
-    shifted_centers = shift_centers(centers, n, r)
     draw_circles(centers, n, r)
+    plt.savefig('figure2.png')
+
+    shifted_centers = shift_centers(centers, n, r)
     draw_circles(shifted_centers, n, r)
-    plt.show()
+    plt.savefig('figure3.png')
+
+    plt.show(block = False)
+
+    from mayavi import mlab
+    draw_cyllindres(centers, n, r, h = n, colormap = 'winter')
+    mlab.show()
